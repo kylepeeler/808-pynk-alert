a6a062ae8e0eae2efca117ac50805a11d855b61a
Kyle Peeler <kylepeeler@Kyles-Personal-16-MBP.local>
2025-09-13 22:42:05 -0700

    fix rollup config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        "808": resolve(__dirname, "808.html"),
+        A5479: resolve(__dirname, "A5479.html"),
+      },
+    },
+  },
 });
